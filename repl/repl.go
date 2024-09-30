8b8a10706245a2cc092c7cf46f067357ab90438f
gangx <gangx6906@gmail.com>
2024-09-30 11:29:16 +0800

    添加REPL的图标

--- repl/repl.go
+++ repl/repl.go
@@ -10,9 +10,22 @@ import (
 )
 
 const PROMPT = "~>" //提示符
+const ICON = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
 
 // Start repl启动，从read读取代码，输出结果给writer
 func Start(in io.Reader, out io.Writer) {
+	fmt.Fprint(out, ICON) //图标输出
 	scanner := bufio.NewScanner(in)
 	for {
 		fmt.Fprint(out, PROMPT)   //提示符输出
